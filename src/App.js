73b0874f8a4fe22175cf2784cfaba112d965af2b
imzuddin <imzuddin@gmail.com>
2024-01-07 01:12:30 -0500

    feat: implemented fixed nav bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactDOM } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import './index.css'
 
+import Nav from './Nav';
+
 function App() {
-  
+
+  const [isActive, setIsActive] = useState(true);
+  const [screenMode, setScreenMode] = useState('dark'); 
+
   return (
     <div className='parentContainer'>
+      <Nav isActive={isActive} screenMode={screenMode}/>
       <div className='subContainer'>
       </div>
       <div className='subContainer'>
       </div>
       <div className='subContainer'>
       </div>
-      <div className='subContainer'>
+      <div className='subContainer' id="experience">
       </div>
     </div>
   );
 }
 
 export default App;
+library.add(fab, fas, far)
