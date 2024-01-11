58f0f56815a64cdd9f1562c876caacb35a48b094
imzuddin <imzuddin@gmail.com>
2024-01-11 00:03:52 -0500

    feat: nav is now responsive

--- src/Nav.js
+++ src/Nav.js
@@ -51,32 +51,55 @@ const Nav = ({
     onProjectsClick
 }) => {
 
+    const [isOpen, setIsOpen] = useState(false);
+    const primaryNav = document.querySelector(".navLinks")
+    const handleNavClick = () => {
+        const visibility = primaryNav.getAttribute('data-visible')
+
+        if (visibility === "true") {
+            primaryNav.setAttribute('data-visible', false)
+            setIsOpen(false)
+        } else {
+            primaryNav.setAttribute('data-visible', true)
+            setIsOpen(true)
+        }
+    }
+
+    
     return (
         isActive && (
             <div className="navParent">
                 <h1 className={`navIcon ${screenMode}`}> IU </h1>
-                <div className="navLinks ">
+
+                <button onClick={handleNavClick} className="mobile-nav-toggle"> 
+                    { isOpen ? (
+                        <FontAwesomeIcon className="nav-toggle-icon" icon={"fa-regular fa-circle-xmark"}/>
+                    ) : (
+                        <FontAwesomeIcon className="nav-toggle-icon" icon={"fa-solid fa-bars"}/>                    
+                    )}
+                </button>
+
+                <div data-visible="false" className="navLinks">
                     <ul className="externalLinks">
-                        {
-                            externalLinks.map(link => {
+                        {externalLinks.map(link => {
                                 return (
-                                <li> 
-                                    <a href={link.link} title={link.title} target="_blank">
-                                        <FontAwesomeIcon 
-                                            icon={link.className} 
-                                            className={`externalLinkIcon ${screenMode}`}
-                                        /> 
-                                    </a>
-                                </li>
+                                    <li> 
+                                        <a href={link.link} title={link.title} target="_blank">
+                                            <FontAwesomeIcon 
+                                                icon={link.className} 
+                                                className={`externalLinkIcon ${screenMode}`}
+                                            /> 
+                                        </a>
+                                    </li>
                                 )
                             })
                         }
                     </ul>
-                </div>
-                <div className="internalLinks">
-                    <Button text={'HOME'} onClick={onHomeClick} status={activeSection === 'home' ? 'active': ''} type={'navButton'}/>
-                    <Button text={'ABOUT'} onClick={onAboutClick} status={activeSection === 'about' ? 'active': ''} type={'navButton'}/>
-                    <Button text={'PROJECTS'} onClick={onProjectsClick} status={activeSection === 'projects' ? 'active': ''} type={'navButton'}/>
+                    <div className="internalLinks">
+                        <Button text={'HOME'} onClick={onHomeClick} status={activeSection === 'home' ? 'active': ''} type={'navButton'}/>
+                        <Button text={'ABOUT'} onClick={onAboutClick} status={activeSection === 'about' ? 'active': ''} type={'navButton'}/>
+                        <Button text={'PROJECTS'} onClick={onProjectsClick} status={activeSection === 'projects' ? 'active': ''} type={'navButton'}/>
+                    </div>
                 </div>
             </div>
         )
